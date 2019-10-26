7327c0dcb601c71a764fdbc23c3f10885cead184
D-Beall <bealdre@bvu.edu>
2019-10-26 13:21:54 -0400

    Created initial mix for project. Named fs for file share...we don't have a name.

---
+++ fs/lib/fs.ex
@@ -0,0 +1,18 @@
+defmodule FS do
+  @moduledoc """
+  Documentation for FS.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FS.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
