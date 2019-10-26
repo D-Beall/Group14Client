1f6f3c06584f32a20e1c85ffffd1fc4df01db503
D-Beall <bealdre@bvu.edu>
2019-10-26 15:59:30 -0400

    Created supervisor, edited spawner to have a start supervisor, and edited test file to try to spawn a supervisor. No functionality besides spawing a supervisor.

--- fs/lib/fs/spawner.ex
+++ fs/lib/fs/spawner.ex
@@ -1,27 +1,35 @@
 defmodule FS.Spawner do
-	use GenServer
+  use GenServer
 
-	@doc """
-	Starts the spawner.
-	"""
+  @doc """
+  Starts the spawner.
+  """
 
-	def start_link(opts) do
-		GenServer.start_link(__MODULE__, :ok, opts)
-	end
-	
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
 
-	@impl true
-	def init(:ok) do
-		{:ok, %{}}
-	end
-	
-	@impl true
-	def handle_call(:request,_from,:state) do
-		{:reply, "Got call."}
-	end
+  @doc """
+  Spawn the supervisor process.
+  """
 
-	@impl true
-	def handle_cast(:request, :state) do
-		{:noreply, "Got cast."}
-	end
+  def start_supervisor(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
+
+  @impl true
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  @impl true
+  def handle_call(:request, _from, :state) do
+    {:reply, "Got call."}
+  end
+
+  @impl true
+  def handle_cast({:create, _name}, _names) do
+		{:ok, supervisor} = KV.Supervisor.start_link([])
+    {:noreply, "Got cast."}
+  end
 end
