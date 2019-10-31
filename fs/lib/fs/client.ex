a916fbc3fccf308a9b3c2a0aff228e06f37c5859
D-Beall <bealdre@bvu.edu>
2019-10-31 16:07:36 -0400

    Some documentation and added cookie to be sent on post request.

--- fs/lib/fs/client.ex
+++ fs/lib/fs/client.ex
@@ -1,7 +1,10 @@
 defmodule FS.Client do
 	def get do
+		#connect to server
 		{:ok, conn} = Mint.HTTP.connect(:http, "xzy3.cs.seas.gwu.edu", 8085)
+		#send request
 		{:ok, conn, request_ref} = Mint.HTTP.request(conn, "GET", "/hello", [], "")
+		#receive message
 		receive do
 			message ->
 				IO.inspect(message, label: :message)
@@ -11,10 +14,17 @@ defmodule FS.Client do
 	end
 
 	def post do
-		list = [key: "value"]
+		#generate UUID/cookie
+		id = UUID.uuid1()
+		cook = UUID.uuid1()#may need to use other UUID function
+		#convert to JSON
+		list = [uuid: id, cookie: cook]
 		{status, result} = JSON.encode(list)
+		#connect to server
 		{:ok, conn} = Mint.HTTP.connect(:http, "xzy3.cs.seas.gwu.edu", 8085)
+		#send post request with json data
 		{:ok, conn, request_ref} = Mint.HTTP.request(conn, "POST", "/post", [{"content-type", "application/json"}], result)
+		#receive message
 		receive do
 			message ->
 				IO.inspect(message, label: :message)
