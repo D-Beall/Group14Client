cbd63b8a7aaa82742ab511b2e3d6f0367080169f
D-Beall <bealdre@bvu.edu>
2019-10-31 15:33:10 -0400

    Edited client module to send post reqeust to our server.

--- fs/lib/fs/client.ex
+++ fs/lib/fs/client.ex
@@ -11,8 +11,10 @@ defmodule FS.Client do
 	end
 
 	def post do
+		list = [key: "value"]
+		{status, result} = JSON.encode(list)
 		{:ok, conn} = Mint.HTTP.connect(:http, "xzy3.cs.seas.gwu.edu", 8085)
-		{:ok, conn, request_ref} = Mint.HTTP.request(conn, "POST", "/post", [{"content-type", "application/json"}], "{'hello': 'world'}")
+		{:ok, conn, request_ref} = Mint.HTTP.request(conn, "POST", "/post", [{"content-type", "application/json"}], result)
 		receive do
 			message ->
 				IO.inspect(message, label: :message)
