71a8cea54bbd5ab30c833ffec8350a79eee118b3
D-Beall <bealdre@bvu.edu>
2019-11-16 11:58:53 -0500

    Added functionality to help command. Begun development of download command to download file. Download command reads in artist, song from user.

--- fs/lib/user-interface.ex
+++ fs/lib/user-interface.ex
@@ -1,25 +1,11 @@
 defmodule UI do
 
-	def handle_response(r) do
-		case r do
-
-		"help" -> IO.puts("List of options.")	
-		get_response(:continue)
-
-		"exit" -> get_response(:exit)
-
-		_-> IO.puts("Unknown command please try again.")
-		get_response(:continue)
-		end
-	end
-
 	def start do
-		FS.Client.server_download()
 		IO.puts("Welcome to audio file share.")
 		IO.puts("Type help for options.")
 		get_response(:continue)
 	end
-	
+
 	def get_response(status) do
 		case status do
 
@@ -32,5 +18,35 @@ defmodule UI do
 		IO.puts("Thank you, applcation shutting down.")	
 		end
 	end
+
+	def handle_response(r) do
+		case r do
+		
+		"download"-> download()
+
+		"help" -> IO.puts("download: Attempted to download audio file.")	
+		IO.puts("help: List possible commands.")	
+		IO.puts("exit: Quit the application.")	
+		get_response(:continue)
+
+		"exit" -> get_response(:exit)
+
+		_-> IO.puts("Unknown command please try again.")
+		get_response(:continue)
+		end
+	end
+
+	def download() do
+	#Function to download song from either server or local network.
+	artist = IO.gets("Enter artist/author name: ")
+	artist = String.trim(artist) 
+	song = IO.gets("Enter song/book title: ")
+	song = String.trim(song) 
+	#Try to search local network for file
+	#Download file from server
+	FS.Client.server_download()
+
+	end
+	
 
 end
